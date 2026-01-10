4494cd468bbda94c900f919df04e9b6ecae2650f
radiant.hh <black5824@naver.com>
2026-01-10 21:40:04 +0900

    add 15829 25304 1676

---
+++ Bronze/15829.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    long long r = 1;
+    const int M = 1234567891;
+
+    int L;
+    cin >> L;
+
+    string s;
+    cin >> s;
+
+
+    long long hash = 0;
+    for (int i = 0; i < L; i++) {
+        int h = s[i] - 96;
+        hash = (hash + h * r) % M;
+        r = (r * 31) % M;
+    }
+
+    cout << hash << "\n";
+
+    return 0;
+}
---
+++ Silver/1676.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int N;
+    cin >> N;
+
+    int cnt = 0;
+    for (int i = 1; i <= N; i++) {
+        if(i % 5 == 0) {
+            cnt++;
+            if(i % 25 == 0) {
+                cnt++;
+            }
+            if(i % 125 == 0) {
+                cnt++;
+            }
+        }
+    }
+    cout << cnt << "\n";
+
+    return 0;
+}
