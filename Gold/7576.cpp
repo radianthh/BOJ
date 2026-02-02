6f3662602d1581996cccffabeee4b2a4e6766c2e
radiant.hh <black5824@naver.com>
2026-02-02 16:46:09 +0900

    modify 7576

--- Gold/7576.cpp
+++ Gold/7576.cpp
@@ -13,21 +13,21 @@ int main() {
     cin.tie(nullptr);
 
     int n, m;
-    cin >> n >> m;
+    cin >> m >> n;
 
     int board[1002][1002];
     int dis[1002][1002];
     memset(dis, -1, sizeof(dis));
     queue<pair<int, int>> Q;
 
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             cin >> board[i][j];
         }
     }
 
-    for (int k = 0; k < m; k++) {
-        for (int e = 0; e < n; e++) {
+    for (int k = 0; k < n; k++) {
+        for (int e = 0; e < m; e++) {
             if(board[k][e] == 1) {
                 Q.push({k, e});
                 dis[k][e] = 0;
@@ -41,7 +41,7 @@ int main() {
             int nx = cur.first + dx[a];
             int ny = cur.second + dy[a];
 
-            if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
             if(dis[nx][ny] != -1 || board[nx][ny] != 0) continue;
             dis[nx][ny] = dis[cur.first][cur.second] + 1;
             board[nx][ny] = 1;
