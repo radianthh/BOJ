8e8320913f676eb7ce8f926b2b71a76d2e051d72
radiant.hh <black5824@naver.com>
2026-02-02 16:40:33 +0900

    add 7576

---
+++ Gold/7576.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <queue>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    int board[1002][1002];
+    int dis[1002][1002];
+    memset(dis, -1, sizeof(dis));
+    queue<pair<int, int>> Q;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> board[i][j];
+        }
+    }
+
+    for (int k = 0; k < m; k++) {
+        for (int e = 0; e < n; e++) {
+            if(board[k][e] == 1) {
+                Q.push({k, e});
+                dis[k][e] = 0;
+            }
+        }
+    }
+    while(!Q.empty()) {
+        pair<int, int> cur = Q.front();
+        Q.pop();
+        for(int a = 0; a < 4; a++) {
+            int nx = cur.first + dx[a];
+            int ny = cur.second + dy[a];
+
+            if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+            if(dis[nx][ny] != -1 || board[nx][ny] != 0) continue;
+            dis[nx][ny] = dis[cur.first][cur.second] + 1;
+            board[nx][ny] = 1;
+
+            Q.push({nx, ny});
+        }
+    }    
+
+    int mx = 0;
+    for (int c = 0; c < m; c++) {
+        for (int d = 0; d < n; d++) {
+            if(board[c][d] == 0) {
+                cout << -1 << "\n";
+                return 0;
+            }
+            mx = max(mx, dis[c][d]);
+        }
+    }
+    cout << mx << "\n";
+
+    return 0;
+
+}
