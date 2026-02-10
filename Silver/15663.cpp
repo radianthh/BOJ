8c1970154ea2347f3717d602d3ad7433651b483f
radiant.hh <black5824@naver.com>
2026-02-10 23:26:38 +0900

    add 15663

---
+++ Silver/15663.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m, a;
+int arr[10];
+bool isused[10];
+vector<int> v;
+
+void func(int k) {
+    if(m == k) {
+        for (int i = 0; i < m; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    int prev = -10001;
+    for (int i = 0; i < n; i++) {
+        if(!isused[i] && v[i] != prev) {
+            prev = v[i];
+            arr[k] = v[i];
+            isused[i] = 1;
+            func(k + 1);
+            isused[i] = 0;
+        }
+    }
+}
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        v.push_back(a);
+    }
+    sort(v.begin(), v.end());
+    func(0);
+
+    return 0;
+}
