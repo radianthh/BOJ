594c46d155ef55d1f5fb3e12c631f08c216b5c70
radiant.hh <black5824@naver.com>
2026-03-04 14:48:47 +0900

    add 11726

---
+++ Silver/11726.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    int D[1001];
+    D[1] = 1; D[2] = 2;
+    for (int i = 3; i <= N; i++) {
+        D[i] = (D[i - 1] + D[i - 2]) % 10007;
+    }
+    cout << D[N] << "\n";
+
+    return 0;
+}
