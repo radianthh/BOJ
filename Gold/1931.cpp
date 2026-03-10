abf5994e0d40a503f60b3aacc85bf916de30f296
radiant.hh <black5824@naver.com>
2026-03-10 13:59:30 +0900

    add 9085 1931

---
+++ Gold/1931.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+bool cmp(pair<int, int> &p1, pair<int, int> &p2) {
+    if(p1.second != p2.second) return p1.second < p2.second;
+    else return p1.first < p2.first;
+}
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    vector<pair<int, int>> v;
+    for (int i = 0; i < N; i++) {
+        int x, y;
+        cin >> x >> y;
+        v.push_back({x, y});
+    }
+    sort(v.begin(), v.end(), cmp);
+
+    int t = v[0].second;
+    int cnt = 1;
+    for (int i = 1; i < N; i++) {
+        if(v[i].first >= t) {
+            t = v[i].second;
+            cnt++;
+        }
+    }
+    cout << cnt << "\n";
+
+    return 0;
+}
